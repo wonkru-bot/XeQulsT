3323db77746e9fded5fd6e1889b82d5c2857e88a
itzforu <86520766+itzforu@users.noreply.github.com>
2021-08-24 14:29:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/itzforu/XEQUIST.git/XEQUIST
+git clone https://github.com/itzforu/XEQUIST.git /XEQUIST
 cd /XEQUIST
 echo "Starting Bot...."
 python3 main.py
