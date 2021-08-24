f49f9be9bfc2a205110c9ae0208c9ee5a7ac6abd
itzforu <86520766+itzforu@users.noreply.github.com>
2021-08-24 14:24:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
+git clone https://github.com/itzforu/XEQUIST.git/XEQUIST
 cd /MusicPlayer
 echo "Starting Bot...."
 python3 main.py
